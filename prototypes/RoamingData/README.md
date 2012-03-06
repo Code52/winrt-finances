e6d4d7a579abccbbb42f7a3e183a7cbc189db87e
Chris Sainty <csainty@hotmail.com>
2012-03-06 21:38:43 +1100

    Added the roaming data prototype.

---
+++ prototypes/RoamingData/README.md
@@ -0,0 +1,7 @@
+Super simple prototype of how remote storage could be handled.
+
+By letting multiple files append to the Finances.Data namespace, we can create self contained providers that adhere to a common interface and can be used interchangeably through the app.  
+See js/Data/roaming.js
+
+
+
---
+++ prototypes/RoamingData/RoamingSettings/js/page.js
@@ -0,0 +1,51 @@
+﻿(function () {
+
+    var dataStore = Finances.Data.roaming;
+    var fileCounter = 0;
+    var fileList = new WinJS.Binding.List();
+    var ui = WinJS.UI;
+    
+    function id(elementId) {
+        return document.getElementById(elementId);
+    }
+
+    function readFile() {
+        dataStore.loadObject("1.json").then(function (obj) {
+            console.log(JSON.stringify(obj));
+        });
+    }
+
+    function createFile() {
+        fileCounter += 1;
+        dataStore.saveObject(fileCounter + ".json", { id: fileCounter, caption: 'test' }).then(function() {
+            console.log('Object ' + fileCounter + ' saved');
+            listFiles();
+        });
+    }
+    
+    function listFiles() {
+        return dataStore.listObjects().then(function (list) {
+            for (var i = 0; i < list.length; i++) {
+                if (fileList.some(function (item) { return item === list[i]; })) { continue; }
+                fileList.push(list[i]);
+            }
+        });
+    }
+
+    function init() {
+        WinJS.UI.processAll();
+        id('readFile').addEventListener('click', readFile);
+        id('createFile').addEventListener('click', createFile);
+        id('listFiles').addEventListener('click', listFiles);
+        
+        var fileListView = id('FileList').winControl;
+        ui.setOptions(fileListView, {
+            itemDataSource: fileList.dataSource
+        });
+        listFiles().then(function () {;
+            fileCounter = fileList.length;
+        });
+    }
+
+    document.addEventListener("DOMContentLoaded", init, false);
+})();
