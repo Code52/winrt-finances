e273d2c93b1aae92292776b6e1c7e8ea0e968e79
Brendan Forster <me@brendanforster.com>
2012-03-07 22:56:21 -0800

    cleanup of template files and renamed to be more specific

--- src/Finances.Win8/js/home.js
+++ src/Finances.Win8/js/home.js
@@ -7,8 +7,8 @@
     ui.Pages.define("/html/home.html", {
         ready: function (element, options) {
             element.querySelector("#transactions").onclick = function () { nav.navigate("/html/transactions.html"); };
-            element.querySelector("#goals").onclick = function () { nav.navigate("/html/transactions.html"); }; // TODO: new page
-            element.querySelector("#dashboard").onclick = function () { nav.navigate("/html/transactions.html"); }; // TODO: new page
+            element.querySelector("#goals").onclick = function () { nav.navigate("/html/goals.html"); }; // TODO: new page
+            element.querySelector("#reports").onclick = function () { nav.navigate("/html/reports.html"); }; // TODO: new page
         },
     });
 })();
