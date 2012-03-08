1f4d12f5acf2354218c933a762a132685348d4ee
Brendan Forster <me@brendanforster.com>
2012-03-07 22:43:54 -0800

    cleanup of the groupedItemsPage html/js files

---
+++ src/Finances.Win8/js/home.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    "use strict";
+
+    var ui = WinJS.UI;
+    var nav = WinJS.Navigation;
+    
+    ui.Pages.define("/html/home.html", {
+        ready: function (element, options) {
+            element.querySelector("#transactions").onclick = function () { nav.navigate("/html/transactions.html"); };
+            element.querySelector("#goals").onclick = function () { nav.navigate("/html/transactions.html"); }; // TODO: new page
+            element.querySelector("#dashboard").onclick = function () { nav.navigate("/html/transactions.html"); }; // TODO: new page
+        },
+    });
+})();
